dfc8bda38e49c2bcea03e85ab339a0b83d775821
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-03 09:45:23 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,28 +1,16 @@
 class Person
-  attr_accessor :partner, :name
+  attr_accessor :name, :partner
  
-  def initialize(name)
-  class Person
-  attr_accessor :partner, :name
-
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
-    if person.class != Person
-      raise PartnerError
-    else
-      person.partner = self
-    end
+    person.partner = self
   end
-  
-  class PartnerError < StandardError
-  end
-
 end
-
+ 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
